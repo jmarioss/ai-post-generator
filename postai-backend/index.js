24a2315138b41c26f82e6a736e5e3c4b95916262
José Mário <josemario.g12001@gmail.com>
2025-06-11 23:08:35 -0300

    feat: sem arquivo env

--- postai-backend/index.js
+++ postai-backend/index.js
@@ -21,3 +21,21 @@ sequelize.authenticate()
   })
   .catch(err => console.error('Erro na conexão com o banco de dados:', err));
 
+
+/*
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const postRoutes = require('./routes/posts');
+const generateRoute = require('./routes/generate');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/posts', postRoutes);
+app.use('/generate', generateRoute);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+*/
